19277f00f26b7e791e0c93fa2003e5ff09350c16
Tadej Kanižar <tadej@ncode.si>
2017-04-21 22:11:20 +0200

    Change default bucket type to the new coldline storage and adjust lifecycle settings accordingly

--- create_bucket.sh
+++ create_bucket.sh
@@ -10,7 +10,7 @@ files=`$GS_UTIL_BINARY_PATH ls $GS_MONTHLY_BUCKET 2>&1`
 exit_code=$?
 if [[ exit_code -eq 1 ]]; then
 	log 'INFO' $log_tag 'Creating monthly bucket'
-	log_contents=`$GS_UTIL_BINARY_PATH mb -c NL -l $GS_BUCKET_LOCATION $GS_MONTHLY_BUCKET 2>&1`
+	log_contents=`$GS_UTIL_BINARY_PATH mb -c coldline -l $GS_BUCKET_LOCATION $GS_MONTHLY_BUCKET 2>&1`
 	log 'INFO' $log_tag "gs_util log: $log_contents"
 	log_contents=`$GS_UTIL_BINARY_PATH lifecycle set lifecycle_config_monthly.json $GS_MONTHLY_BUCKET 2>&1`
 	log 'INFO' $log_tag "gs_util log: $log_contents"
@@ -20,7 +20,7 @@ files=`$GS_UTIL_BINARY_PATH ls $GS_WEEKLY_BUCKET 2>&1`
 exit_code=$?
 if [[ exit_code -eq 1 ]]; then
 	log 'INFO' $log_tag 'Creating weekly bucket'
-	log_contents=`$GS_UTIL_BINARY_PATH mb -c NL -l $GS_BUCKET_LOCATION $GS_WEEKLY_BUCKET 2>&1`
+	log_contents=`$GS_UTIL_BINARY_PATH mb -c coldline -l $GS_BUCKET_LOCATION $GS_WEEKLY_BUCKET 2>&1`
 	log 'INFO' $log_tag "gs_util log: $log_contents"
 	log_contents=`$GS_UTIL_BINARY_PATH lifecycle set lifecycle_config_weekly.json $GS_WEEKLY_BUCKET 2>&1`
 	log 'INFO' $log_tag "gs_util log: $log_contents"
